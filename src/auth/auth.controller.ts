b33729be207a84c5a8d6922102ef09701565b65e
Bright Munemu <brightmunemu@gmail.com>
2025-12-14 03:33:27 +0100

    create docker compose

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.services';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  //   authService: AuthService;
+  constructor(private authService: AuthService) {
+    // this.authService = authService;
+    this.authService.test();
+  }
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  test() {}
+  signup() {
+    return { message: 'User registered successfully!' };
+  }
+
+  signin() {
+    return { message: 'Logged in successfully!' };
+  }
+}
