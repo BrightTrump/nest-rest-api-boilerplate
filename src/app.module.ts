c514e95de8f277f6204f38c4a16382b4a629098c
Bright Munemu <brightmunemu@gmail.com>
2026-02-16 08:24:49 +0100

    create employees resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UserModule, AuthModule, DatabaseModule],
+  imports: [UserModule, AuthModule, DatabaseModule, EmployeesModule],
   providers: [UsersService],
 })
 export class AppModule {}
